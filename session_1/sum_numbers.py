f235f7489da77492469fd7678f7cf6c45df93e81
Hamzeh <h.qorbani7@gmail.com>
2023-08-23 22:25:12 +0430

    session_1: variable,value,print,int,str

---
+++ session_1/sum_numbers.py
@@ -0,0 +1,5 @@
+# variable = value
+# for comment in python press: ctrl + /
+a = 2 # integer int
+b = 3 # integer int
+print(a + b)
---
+++ session_1/welcome.py
@@ -0,0 +1,4 @@
+name = "Hamzeh" # string str
+family = "Qorbani" # string str
+print("Welcome" , name , family)
+print("Welcome" + name + family)

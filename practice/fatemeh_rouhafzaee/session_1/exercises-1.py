d3097689c60492f4d6ce8904193a8a7452f5af9f
Hamzeh <h.qorbani7@gmail.com>
2023-08-30 04:49:50 +0430

    practice answers from fatemeh_rouhafzaee

---
+++ practice/fatemeh_rouhafzaee/session_1/exercises-1.py
@@ -0,0 +1,3 @@
+name=input('please enter your name:')
+last_name=input('please enter your last name:')
+print("Dear" ,name , last_name ,"welcome to Python level 1 class.")
---
+++ practice/fatemeh_rouhafzaee/session_3/test_3.py
@@ -0,0 +1,8 @@
+user_age =int(input("please enter your age:"))
+if 13 <= user_age <= 19:
+    print("you are a teenager.")
+elif user_age < 13:
+    print("you are a child.")
+elif  user_age > 19:
+    print("you are a adult.")
+

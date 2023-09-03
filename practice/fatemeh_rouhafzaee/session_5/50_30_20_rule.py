88b32da3406cebe938e2dd564ce605714d0791d8
Hamzeh <h.qorbani7@gmail.com>
2023-09-03 17:44:52 +0430

    practice answer from fatemeh_rouhafzaee and kian_mojarad

---
+++ practice/fatemeh_rouhafzaee/session_5/50_30_20_rule.py
@@ -0,0 +1,7 @@
+income = int(input("enter your income:"))
+Need = income * 50/100
+Wants = income * 30/100
+save = income * 20/100
+print("need:" , Need)
+print("Want:" , Wants)
+print("Save:" , save)

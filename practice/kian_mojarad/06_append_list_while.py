87f2cae129428122fa60a9e73d1d1502494c5028
Hamzeh <h.qorbani7@gmail.com>
2023-09-06 14:06:14 +0330

    practice answer: 06_append_list_while

---
+++ practice/kian_mojarad/06_append_list_while.py
@@ -0,0 +1,12 @@
+names = ["sina" , "arian" , "kamran"]
+sal_tavalod = "sal_tavalod" , 1362 , 1368 , 1370
+print(names)
+print()
+print(sal_tavalod)
+print()
+for item in names:
+    print(item)
+    if item == "sina":
+        print(item , "is a kapitan")
+    else:
+        print(item , "is a khadame")

957af3c3605bf130610a683031764caa0b1063f6
Hamzeh <h.qorbani7@gmail.com>
2023-09-06 13:23:34 +0330

    practice answer : 50_30_20 from kian_mojarad

---
+++ practice/kian_mojarad/50_30_20.py
@@ -0,0 +1,7 @@
+income = int(input("dar amad khod ra vared konid:"))
+niaz = income * 45/100
+khasteha = income * 30/100
+zakhire = income * 25/100
+print("niaz:" , niaz)
+print("khasteha:" , khasteha)
+print("zakhire:" , zakhire)

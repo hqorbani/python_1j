59cebe014bd4a0ef942b7d063ed7224e255594ea
Hamzeh <h.qorbani7@gmail.com>
2023-09-18 22:03:08 +0330

    session_8:intro_dic

---
+++ session_8/intro_dic.py
@@ -0,0 +1,22 @@
+my_list = ["Hamzeh" , "Mina" , "Fatemeh"]
+print("list item:",my_list[2])
+numbers = [54,56,87,98]
+# Dictionary
+person = {"name":"Hazmeh" , "family":"Qorbani" , "mobile":"09159862132" , "age": 39 }
+
+person = {
+    "name":"Hazmeh" ,
+    "family":"Qorbani" ,
+    "mobile":"09159862132" ,
+    "age": 39
+    }
+print("dic items:")
+print(person["name"])
+print(person["family"])
+
+persons = {
+    "names":["Hamzeh" , "Mina" , "Fatemeh"],
+    "family":["Qorbani" , "Nabavi" , "Rouhafzaee"]
+}
+print("neseted dic:")
+print(persons["names"][2])
